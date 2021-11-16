2f30732cb3251e816caa8eba518b3ba680419080
Laras310 <nurullarasati310@gmail.com>
2021-11-16 17:14:14 +0700

    update no 1

--- praktikum_mg5/no1.php
+++ praktikum_mg5/no1.php
@@ -1,43 +1,31 @@
 <?php
-function tambah($x,$y){
-    return $x + $y;
-}
-
-function kurang($x,$y){
-    return $x - $y;
-}
-
-function bagi($x,$y){
-    return $x / $y;
-}
-
-function kali($x,$y){
-    return $x * $y;
-}
-
-function mod($x,$y){
-    return $x %$y;
+function kalku($x,$y,$operator){
+    if($operator=="+") return $x + $y;
+    else if ($operator=="-") return $x - $y;
+    else if ($operator=="/") return $x / $y;
+    else if ($operator=="*") return $x * $y;
+    else if($operator=="%") return $x%$y;
 }
 
 $a=10;
-$b=5;
+$b=3;
 echo "PENJUMLAHAN<br>";
 echo "Operator: +<br>";
-echo "Hasil : ", tambah($a,$b),"<br><br>";
+echo "Hasil : ", kalku($a,$b,"+"),"<br><br>";
 
 echo "PENGURANGAN<br>";
 echo "Operator: -<br>";
-echo "Hasil : ", kurang($a,$b),"<br><br>";
+echo "Hasil : ", kalku($a,$b,"-"),"<br><br>";
 
 echo "PERKALIAN<br>";
 echo "Operator: *<br>";
-echo "Hasil : ", kali($a,$b),"<br><br>";
+echo "Hasil : ", kalku($a,$b,"*"),"<br><br>";
 
 echo "PEMBAGIAN<br>";
 echo "Operator: /<br>";
-echo "Hasil : ", bagi($a,$b),"<br><br>";
+echo "Hasil : ", kalku($a,$b,"/"),"<br><br>";
 
 echo "MODULUS<br>";
 echo "Operator: %<br>";
-echo "Hasil : ", mod($a,$b),"<br";
+echo "Hasil : ", kalku($a,$b,"%"),"<br";
 ?>
